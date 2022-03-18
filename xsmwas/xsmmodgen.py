930dd27ac890ba797ff57ef372afea1a2c5c21f7
theModernDaedalus <69039654+theModernDaedalus@users.noreply.github.com>
2022-03-18 20:19:59 +0530

    Add files via upload

---
+++ xsmwas/xsmmodgen.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import pandas as pd
+from sklearn.cluster import *
+import pickle
+
+dataset = pd.read_csv('final_data.csv')
+relevantDataset = dataset.iloc[:, 3:11]
+relevantDataset = relevantDataset.drop(['Start Time (s)'], axis = 1)
+relevantDataset = relevantDataset.drop(['End Time (s)'], axis = 1)
+relevantDataset = relevantDataset.drop(['Duration (s)'], axis = 1)
+
+model = KMeans(n_clusters = 6, random_state = 1).fit(relevantDataset)
+
+
+pkl_file = "xsm_model.pkl"
+with open(pkl_file, 'wb') as file:
+    pickle.dump(model, file)
+
+#with open(pkl_file, 'rb') as file:
+#    pkl_model = pickle.load(file)
+    
+#arr = np.array([97.5, 37.2, 0.003, 5625, 4364])
+#arr = arr.reshape(1, -1)
+
+
